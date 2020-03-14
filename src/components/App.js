e246703e6ee720c6a8c5fb2e5b983c88be1eb388
abheist <abhishek291093@gmail.com>
2020-03-15 00:10:38 +0530

    AKS | Authentication | done

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Switch>
 					<Route exact path="/" component={LinkList} />
 					<Route exact path="/create" component={CreateLink} />
+					<Route exact path="/login" component={Login} />
 				</Switch>
 			</div>
 		</div>
--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { createHttpLink } from 'apollo-link-http';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
 
 const httpLink = createHttpLink({
 	uri: 'http://localhost:4000'
 });
 
+const authlink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ''
+		}
+	};
+});
+
 const client = new ApolloClient({
-	link: httpLink,
+	link: authlink.concat(httpLink),
 	cache: new InMemoryCache()
 });
 
